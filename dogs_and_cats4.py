8525f7d55701039ded9c83a5975842924227e4f0
Priyanshi Omer <54883959+priomer@users.noreply.github.com>
2021-01-14 15:23:32 +0530

    Add files via upload
    Added the python classification file code

---
+++ dogs_and_cats4.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 21:24:57 2020
+
+@author: ocn
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.preprocessing import image
+
+# for plotting images (optional)
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+    plt.tight_layout()
+    plt.show()
+    
+
+# getting data
+base_dir = 'E:\AVRN_Report\images'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+train_cats = os.path.join(train_dir, 'cats')
+train_dogs = os.path.join(train_dir, 'dogs')
+validation_cats = os.path.join(validation_dir, 'cats')
+validation_dogs = os.path.join(validation_dir, 'dogs')
+
+num_cats_tr = len(os.listdir(train_cats))
+num_dogs_tr = len(os.listdir(train_dogs))
+num_cats_val = len(os.listdir(validation_cats))
+num_dogs_val = len(os.listdir(validation_dogs))
+
+total_train = num_cats_tr + num_dogs_tr
+total_val = num_cats_val + num_dogs_val
+
+BATCH_SIZE = 32
+IMG_SHAPE = 150 # square image
+
+
+#generators
+
+#prevent memorization
+train_image_generator = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+    )
+
+validation_image_generator = ImageDataGenerator(
+    rescale=1./255)
+
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                           class_mode='binary')
+
+val_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
+                                                           directory=validation_dir,
+                                                           shuffle=False,
+                                                           target_size=(IMG_SHAPE, IMG_SHAPE),
+                                                           class_mode='binary')
+images = [train_data_gen[0][0][0] for i in range(5)]
+plotImages(images)
+
+
+model = Sequential()
+# Conv2D : Two dimentional convulational model.
+# 32 : Input for next layer
+# (3,3) convulonational windows size
+model.add(Conv2D(32, (3, 3), input_shape=(IMG_SHAPE, IMG_SHAPE,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Dropout(0.5)) # 1/2 of neurons will be turned off randomly
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+    
+model.add(Dense(2, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+
+EPOCHS = 10
+
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(total_train / float(BATCH_SIZE))),
+    epochs=EPOCHS,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(total_val / float(BATCH_SIZE)))
+    )
+
+
+# analysis
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
